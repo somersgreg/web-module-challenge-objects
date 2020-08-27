9c1fd0547730dff509818208fd12a05fd31de853
Greg Somers <somersgreg@yahoo.com>
2020-08-27 19:39:17 -0400

    > Starting Objects

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function createMenuItem(name, cost, category){
 
 
 
-/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
+/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter.
 
-Your method should accept: 
+Your method should accept:
 
 (1) A string (teacher, student, or public)
 
-and should return a number. 
+and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
@@ -54,37 +54,37 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 (2) a number which is the desired index in the array.
 
 and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
- * 
+ *
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
     /* code here */
   }
-  
+
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
 getLastReview should accept:
-  (1) an array of objects 
-  
+  (1) an array of objects
+
 and should return a string in the format `name} gave the restaurant a {rating}, and their feedback was: {feedback}`
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
 function getLastReview(/* code here */) {
     /* code here */
-  } 
+  }
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
-/** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
+/** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept:
 
   (1) An array of objects
   (2) A rating
 
-  and should return an array of objects. 
+  and should return an array of objects.
 
   For example, invoking getReviewByRating(reviews, 4) would return [{name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
@@ -95,14 +95,14 @@ function getLastReview(/* code here */) {
     /* code here */
   }
 
-  
-/** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
-  
-Your function should accept: 
+
+/** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words.
+
+Your function should accept:
 
   (1) An array of objects
 
-and should return an array of objects. 
+and should return an array of objects.
 
   For example, invoking getLongReviews(reviews) would return [
     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
@@ -112,15 +112,15 @@ and should return an array of objects.
   function getLongReviews(/* code here */) {
     /* code here */
   }
-  
 
-/* STRETCH 3:  This challenge is not related to the data above! 
 
-Write a function called carMarker 
+/* STRETCH 3:  This challenge is not related to the data above!
+
+Write a function called carMarker
 
 Your function should accept:
 
-(1) a single odometer argument (a number) 
+(1) a single odometer argument (a number)
 
 and return an object.
 
@@ -134,5 +134,5 @@ The returned object should have the following characteristics:
 
 function carMaker(/* code here */) {
     /* code here */
-    
+
 }
